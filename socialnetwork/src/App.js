8407073b8c6af85e532f845024ef7d14a317c658
unknown <vladancupric@gmail.com>
2022-05-15 09:49:35 +0200

    New commits

--- socialnetwork/src/App.js
+++ socialnetwork/src/App.js
@@ -1,20 +1,20 @@
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize";
 import './App.css';
-import {BrowserRouter, Router, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import UserForm from "./components/UserForm/UserForm";
 import React from "react";
 
 function App() {
   return (
-   <Router>
+   <Routes>
    <Route exact path="/" component={UserForm} />
    <React.Fragment>
      <div>
 
      </div>
    </React.Fragment>
-   </Router>
+   </Routes>
   );
 }
 
