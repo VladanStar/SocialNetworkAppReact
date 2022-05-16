def4396982686a813a02c5e2db357d7bb7dcb7b1
unknown <vladancupric@gmail.com>
2022-05-16 08:38:16 +0200

    New commit login and register done

--- socialnetwork/src/App.js
+++ socialnetwork/src/App.js
@@ -1,31 +1,33 @@
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize";
 import './App.css';
-import {BrowserRouter, Routes, Switch, Route, Router} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import { UserForm } from "./components/UserForm/UserForm";
 import React from "react";
 
 function App() {
   return (
-    <Router>
-    {<Route exact path="/" component={UserForm} /> }
+    <BrowserRouter>
+    <Routes>
+    {<Route exact path="/" element={<UserForm/>} /> }
  
     <React.Fragment>
-      <div className="page-container">
-        <div className="content">
+      {/* <div className="page-container">
+        <div className="content"> */}
           {/* <Header /> */}
           {/* <Route exact path="/feed" component={Feed} /> */}
           {/* <Route exact path="/feed/post/:id" component={SinglePost} />
           <Route exact path="/profile" component={Profile} /> */}
           {/* <Route exact path="/people" component={People} />
           <Route exact path="/people/:id" component={SingleUser} /> */}
-        </div>
+        {/* </div>
         {/* <Footer /> */}
-      </div>
+      {/* </div> */} 
     </React.Fragment>
-  </Router>
-  )
+  </Routes>
+  </BrowserRouter>
+  );
 }
 
 export default App;
